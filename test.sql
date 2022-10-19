829adc7552fabe963ab381a67d06c62ccf866136
denis lussier <denis@lussier.io>
2022-10-18 20:34:03 -0400

    initial commit

---
+++ test.sql
@@ -0,0 +1,30 @@
+
+-- a comment
+
+/* single line comment */
+
+/*
+ * multi-line comment
+ */
+
+set termout off
+sEt termout on
+
+
+DROP TABLE IF EXISTS t1;
+
+DROP TABLE t1;
+
+CREATE TABLE t1 (
+  c1 text,
+  c2 integer
+);
+
+SELECT * FROM t1;
+
+INSERT INTO t1 
+  VALUES ('Denis', 1);
+INSERT INTO t1 VALUES ('Gary',  2);
+INSERT INTO t1 VALUES ('Chris', 3);
+
+SELECT * FROM t1;
